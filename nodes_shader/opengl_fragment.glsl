e0fffb5c4cdf82f5d922d34b375d4e7dd1cc9a97
Gefüllte Taubenbrust <72752000+GefullteTaubenbrust2@users.noreply.github.com>
2023-10-31 11:12:11 +0100

    Update opengl_fragment.glsl

--- nodes_shader/opengl_fragment.glsl
+++ nodes_shader/opengl_fragment.glsl
@@ -558,7 +558,7 @@ void main(void)
 		for (int i = 0; i < 20; i++) {
 			float f = (float(i) + bias) / 20.;
 			vec3 ray_position = ray_origin + viewVec * f * f * ray_length;
-			ray_intensity += getGodRay(ray_position) * 0.002 * float(2 * i + 1);
+			ray_intensity += getGodRay(ray_position) * 0.00002 * float(2 * i + 1) * ray_length;
 		}
 		ray_intensity *= pow(max(-dot(v_LightDirection, viewVec), 0.), 4.);
 
